5e71f69b17fdbaa709c12665cc0bc5482ced8d4b
lfazzion <lowellfazzion@gmail.com>
2024-09-14 09:55:20 -0300

    Ajustes para implantacao no GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,7 +7,7 @@ import Module from './pages/Module';
 
 function App() {
   return (
-    <Router basename="/estatisticaEnsinoMedio">
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
